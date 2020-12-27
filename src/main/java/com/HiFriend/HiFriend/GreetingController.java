f6184054b8d463eb130f837aed834ec96d5608d7
josuepalominoperez <oscar.palomino.perez@gmail.com>
2020-12-27 07:19:11 +0000

    GreetingController updated

--- src/main/java/com/HiFriend/HiFriend/GreetingController.java
+++ src/main/java/com/HiFriend/HiFriend/GreetingController.java
@@ -6,10 +6,9 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
 
-import javax.swing.JOptionPane;
+
+
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -17,7 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
