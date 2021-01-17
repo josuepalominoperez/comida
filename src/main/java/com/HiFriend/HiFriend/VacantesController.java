6aabb06fd4f0e9c272e875aee5e4dcf28cfa2137
josuepalominoperez <oscar.palomino.perez@gmail.com>
2021-01-17 02:56:31 +0000

    Seccion borrar con RequestParam agregado

--- src/main/java/com/HiFriend/HiFriend/VacantesController.java
+++ src/main/java/com/HiFriend/HiFriend/VacantesController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/vacantes")
@@ -12,9 +13,17 @@ public class VacantesController {
 
     @GetMapping("/view/{id}")
     public String verDetalle(@PathVariable("id") int idVacante , Model model){
-        System.out.println("IdVacante"+idVacante);
+        System.out.println("idVacante: "+idVacante);
         model.addAttribute("idVacante", idVacante);
         return "detalles";
     }
+
+    @GetMapping("/delete")
+    public String borrando(@RequestParam("id") int idVacante, Model model){
+        System.out.println("Borrando vacante con id: "+idVacante);
+        model.addAttribute("id", idVacante);
+        return "delete";
+    }
+
     
 }
