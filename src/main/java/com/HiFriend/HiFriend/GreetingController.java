70e5b5b87e1bf954193c4053b6d342417eda6f23
josuepalominoperez <oscar.palomino.perez@gmail.com>
2022-02-14 07:27:12 +0000

    url modified for valentineday establish version part 2

--- src/main/java/com/HiFriend/HiFriend/GreetingController.java
+++ src/main/java/com/HiFriend/HiFriend/GreetingController.java
@@ -117,7 +117,7 @@ public class GreetingController {
 		return "notepad";
 	}
 	
-	@PostMapping("/valentineday")
+	@RequestMapping("/valentineday")
 	public String valentineday(@RequestParam("nombre") String nombre , @RequestParam("descripcion") String descripcion , Model model) {
 		model.addAttribute("nombre", nombre);	
 		model.addAttribute("descripcion", descripcion);
